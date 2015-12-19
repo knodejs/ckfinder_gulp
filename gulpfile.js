5f1d7591997046f7041b81ba31a738049376214c
Narongchai Thongkongkeaw <kerlang.2515@gmail.com>
2015-12-19 12:48:49 +0700

    update ckfinder

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,16 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect-php');
-var browserSync = require('browser - sync');
+var browserSync = require('browser-sync');
 
 
-gulp.task('connect', function() {
-  connect.server({
-    hostname: '0.0.0.0',
-    bin: 'D:/Developer/WebServer/php-7.0.1-nts-Win32-VC14-x64',
-    ini: 'D:/Developer/WebServer/php-7.0.1-nts-Win32-VC14-x64/php.ini',
-    port: 8000
-  });
-});
+gulp.task('default', function() {
+    connect.server({
+        hostname: '0.0.0.0',
+        bin: 'D:/Developer/WebServer/php-7.0.1-nts-Win32-VC14-x64/php',
+        ini: 'D:/Developer/WebServer/php-7.0.1-nts-Win32-VC14-x64/php.ini',
+        port: 8000
 
-gulp.task('default',['connect'], function() {
-    connect.server({}, function() {
+    }, function() {
         browserSync({
             proxy: '127.0.0.1:8000'
         });
@@ -23,4 +20,3 @@ gulp.task('default',['connect'], function() {
         browserSync.reload();
     });
 });
-
